24d14e4ddbf29106b5f845697603037c3c562be7
UaMV <UaMV@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-29 04:44:21 +0000

    Adding v2.0
    git-svn-id: https://plugins.svn.wordpress.org/appear-in-wp/trunk@974998 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- appear-in-wp.php
+++ appear-in-wp.php
@@ -3,7 +3,7 @@
  * Plugin Name: appear.in WP
  * Plugin URI: http://vandercar.net/wp/appear-in-wp
  * Description: Adds appear.in rooms to your site via shortcode
- * Version: 1.8
+ * Version: 2.0
  * Author: UaMV
  * Author URI: http://vandercar.net
  *
@@ -15,7 +15,7 @@
  * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  *
  * @package appear.in WP
- * @version 1.8
+ * @version 2.0
  * @author UaMV
  * @copyright Copyright (c) 2013, UaMV
  * @link http://vandercar.net/wp/appear-in-wp
@@ -26,9 +26,10 @@
  * Define constants.
  */
 
-define( 'AIWP_VERSION', '1.8' );
+define( 'AIWP_VERSION', '2.0' );
 define( 'AIWP_DIR_PATH', plugin_dir_path( __FILE__ ) );
 define( 'AIWP_DIR_URL', plugin_dir_url( __FILE__ ) );
+! defined( 'AIWP_SHOW_INVITE' ) ? define( 'AIWP_SHOW_INVITE', TRUE ) : FALSE;
 
 /**
  * Include files.
@@ -69,7 +70,7 @@ class Appear_In_WP {
 	/**
 	 * Custom plugin settings
 	 *
-	 * @since    4.0
+	 * @since    1.0
 	 *
 	 * @var      object
 	 */
@@ -98,21 +99,6 @@ class Appear_In_WP {
 
 		// add the shortcode
 		add_shortcode( 'appear_in', array( $this, 'display_shortcode' ) );
-
-		// include the ajax library on the front end
-		add_action( 'wp_head', array( &$this, 'add_ajax_library' ) );
-
-		// ajax action callback for sending email invites
-		add_action( 'wp_ajax_aiwp_invite', array( &$this, 'email_invites' ) );  // if logged-in
-		add_action( 'wp_ajax_nopriv_aiwp_invite', array( &$this, 'email_invites' ) );  // if not logged-in
-
-		// ajax action callback for counting sessions
-		add_action( 'wp_ajax_aiwp_session', array( &$this, 'count_session' ) );  // if logged-in
-		add_action( 'wp_ajax_nopriv_aiwp_session', array( &$this, 'count_session' ) );  // if not logged-in
-
-		// ajax action callback for counting accepted invitations
-		add_action( 'wp_ajax_aiwp_direct_session', array( &$this, 'count_direct_session' ) );  // if logged-in
-		add_action( 'wp_ajax_nopriv_aiwp_direct_session', array( &$this, 'count_direct_session' ) );  //if not logged-in
 
 	} // end __construct
 
@@ -184,26 +170,6 @@ class Appear_In_WP {
 	} // end add_stylesheets_and_javascript
 
 	/**
-	 * Adds the WordPress Ajax Library to the frontend.
-	 *
-	 * @since    1.0
-	 */
-	public function add_ajax_library() {
-	
-		$scheme = is_ssl() ? 'https' : 'http';
-
-		// build the inline script
-		$html = '<script type="text/javascript">';
-			$html .= 'var ajaxurl = "' . admin_url( 'admin-ajax.php', $scheme ) . '"';
-			
-		$html .= '</script>';
-	
-		// echo the inline script
-		echo $html;
-	
-	} // end add_ajax_library
-
-	/**
 	 * Returns the shortcode content
 	 *
 	 * @since    1.0
@@ -216,9 +182,6 @@ class Appear_In_WP {
 		extract( shortcode_atts( array(
 			'room' => '',
 			'type' => 'public',
-			'public_invites' => NULL,
-			'private_invites' => NULL,
-			'post_invites' => NULL,
 		), $atts ) );
 
 		// push the shortcode defined rooom types to an array
@@ -233,32 +196,28 @@ class Appear_In_WP {
 			$custom_room_name = get_option( 'aiwp_public_room' );
 		}
 
-		// put allowable invites from shortcode into array
-		$invites = array(
-			'public' => $public_invites,
-			'private' => $private_invites,
-			'post' => $post_invites,
-			);
-
-		// if null, get from custom options
-		foreach ( $invites as $room_type => $invite_count ) {
-			if ( is_null( $invites[ $room_type ] ) ) {
-				$invites[ $room_type ] = $this->options['invites'][ $room_type ];
-			}
-		}
-
 		// build room selection wrapper
-		$html = '<div id="aiwp-room-type-selection">';
+		// Add styling for iconset
+		$html = '<style type="text/css">
+					#aiwp-room-type-selection div button,
+					#aiwp-room-type-selection div div button {
+						background: ' . $this->options['color'] . ';
+					}			
+					#aiwp-room-type-selection div button:hover {
+						background: ' . $this->hex_color_mod( $this->options['color'], -16 ) . ';
+					}
+				</style>';
+
+		$html .= '<div id="aiwp-room-type-selection">';
 
 			foreach ( $aiwp_room_types as $room_type ) {
 
 				$room_button_text = isset( $atts[ $room_type . '_room_button' ] ) ? $atts[ $room_type . '_room_button' ] : ucfirst( $room_type ) . ' Room';
-				$invite_button_text = isset( $atts[ $room_type . '_invite_button' ] ) ? $atts[ $room_type . '_invite_button' ] : 'Send Invitations & Enter ' . ucfirst( $room_type ) . ' Room';
-
-				// display public room button, depending on which types of rooms are to be displayed
-				$html .= '<div id="aiwp-' . $room_type . '" style="width:' . ( 100 / count( $aiwp_room_types ) ) . '%"><button id="aiwp-select-' . $room_type . '-room" data-room-type="' . $room_type . '" data-room-invites="';
-					$html .= (int) $invites[ $room_type ] > 0 ? 'enabled"' : 'disabled"';
-					$html .= '>' . apply_filters( 'aiwp_room_button', $room_button_text, $room_type ) . '</button>' . $this->invite_form( (int) $invites[ $room_type ], $room_type, $invite_button_text ) . '</div>';
+				
+				// display room buttons
+				$html .= '<div id="aiwp-' . $room_type . '" style="width:' . ( 100 / count( $aiwp_room_types ) ) . '%">';
+					$html .= '<button id="aiwp-select-' . $room_type . '-room" data-room-type="' . $room_type . '">' . $room_button_text . '</button>';
+				$html .= '</div>';
 			
 			}
 
@@ -268,7 +227,23 @@ class Appear_In_WP {
 		$html .= '<span id="appearin-incompatibility" style="display:none;">' . apply_filters( 'aiwp_unsupported_browser_message', 'It appears your browser is not capable of displaying this content. Try connecting with Chrome, Firefox, or Opera.' ) . '</span>';
 
 		// include appearin iframe populated by API
-		$html .= '<iframe id="appearin-room" data-room-name="' . $custom_room_name . '" data-security="' . wp_create_nonce( 'aiwp-action-on_' . get_option( 'aiwp_public_room' ) ) . '"></iframe>';
+		$html .= '<iframe id="appearin-room" data-room-name="' . $custom_room_name . '"></iframe>';	
+
+		if ( AIWP_SHOW_INVITE ) {
+
+			$html .= '<div id="aiwp-invites" style="display:none;">';
+
+				// add social invites
+				$html .= '<div class="aiwp-invite-buttons">';
+					$html .= '<a href="#" id="aiwp-invite-facebook" class="aiwp-social" target="_blank">Invite via Facebook</a>';
+					$html .= '<a href="#" id="aiwp-invite-twitter" class="aiwp-social" target="_blank">Invite via Twitter</a>';
+					$html .= '<a href="#" id="aiwp-invite-email" class="aiwp-social" target="_blank">Invite via Email</a>';
+				$html .= '</div>';
+
+			$html .= '</div>';
+
+		}
+
 		$html .= '<div id="appearin-room-labels">';
 			$html .= '<div id="appearin-room-label-external"></div>';
 			$html .= '<div id="appearin-room-label"></div>';
@@ -279,176 +254,31 @@ class Appear_In_WP {
 	} // end display_shortcode
 
 	/**
-	 * Build an invitation form
+	 * Change the brightness of the passed in color
 	 *
-	 * @since    1.0
-	 *
-	 * @return   string   HTML content for invite form.
+	 * $diff should be negative to go darker, positive to go lighter and
+	 * is subtracted from the decimal (0-255) value of the color
+	 * 
+	 * @param string $hex color to be modified
+	 * @param string $diff amount to change the color
+	 * @return string hex color
 	 */
-	public function invite_form( $invites = 0, $room_type, $invite_button_text ) {
-
-		// if invites enabled for room type, then build a hidden form
-		// otherwise, include hidden content to trigger room launch
-		if ( $invites > 0 ) {
-
-			$html = '<div id="aiwp-' . $room_type . '-invite-form" style="display:none;">';
-
-				// include replacement button for invite submission and room entrance
-				$html .= '<button id="aiwp-send-' . $room_type . '-invites" data-room-type="' . $room_type . '" tabindex="12">' . apply_filters( 'aiwp_invite_button', $invite_button_text, $room_type ) . '</button>';
-				
-				// inform of optional invites (delay hide with js)
-				$html .= '<span>Optionally Invite</span>';
-
-				// include input for users name & email
-				$html .= '<input type="text" id="aiwp-' . $room_type . '-username" name="aiwp_username" placeholder="Your Name" value="" tabindex="3"></input>';
-				$html .= '<input type="text" id="aiwp-' . $room_type . '-email" name="aiwp_email" placeholder="Your Email" value="" tabindex="4"></input>';
-				
-				// include some number of inputs for invitation emails (defined by custom settings)
-				for ( $i=1; $i<($invites+1); $i++ ) {
-					$html .= '<input type="text" id="aiwp-' . $room_type . '-invite-' . $i . '" name="aiwp_' . $room_type . '_invite_' . $i . '" placeholder="Email (' . $i . '/' . $invites . ')"';
-					$html .= $i>1 ? ' style="display:none;"' : ''; // ensure the first field is displayed
-					$html .= ' value="" tabindex="' . ( 4 + $i ) . '"></input>';
-				}
-
-			$html .= '</div>';
-
-		} else {
-
-			$html = '<span id="aiwp-launch-' . $room_type . '" style="display:none;"></span>';
-
-		} // end if
-
-		return $html;
-	}
-
-	/**
-	 * Callback for ajax send email invites
-	 *
-	 * @since    1.0
-	 */
-	public function email_invites() {
-		
-		if ( check_ajax_referer( 'aiwp-action-on_' . get_option( 'aiwp_public_room' ), 'aiwp_security' ) ) {
-			// get the submitted parameters
-			$aiwp_username = $_POST['aiwp_username'];
-			$aiwp_email = $_POST['aiwp_email'];
-			$aiwp_room_url = $_POST['aiwp_room_url'];
-			$aiwp_room = $_POST['aiwp_room'];
-			$aiwp_room_type = $_POST['aiwp_room_type'];
-			$aiwp_invites = array();
-
-			// get the submitted emails and fill array
-			for( $i=1; $i<8; $i++ ) {
-				( isset( $_POST[ 'aiwp_invite_' . $i ] ) && '' != $_POST[ 'aiwp_invite_' . $i ] ) ? $aiwp_invites[] = $_POST[ 'aiwp_invite_' . $i ] : FALSE;
+	public function hex_color_mod($hex, $diff) {
+		$rgb = str_split(trim($hex, '# '), 2);
+	 
+		foreach ($rgb as &$hex) {
+			$dec = hexdec($hex);
+			if ($diff >= 0) {
+				$dec += $diff;
 			}
-
-			// if no invite emails set, then return true to enter room
-			// otherwise, process email invites
-			if ( empty( $aiwp_invites ) ) {
-				echo TRUE;
-			} else {
-
-				// validate invite email addresses
-				foreach ( $aiwp_invites as $email ) {
-
-					// if any fail, then return false triggering error message
-					if ( ! is_email( $email ) ) {
-						echo FALSE;
-						die();
-					}
-
-				}
-
-				// if from email set and valid, build header
-				// otherwise, set empty header
-				if ( '' != $aiwp_email && is_email( $aiwp_email ) ) {
-					$header = 'From: ';
-					$header .= '' != $aiwp_username ? $aiwp_username : '';
-					$header .= '<' . $aiwp_email . '>';
-				} else {
-					$header = '';
-				}
-
-				// build the message content
-				$message = 'You have been invited';
-				$message .= '' != $aiwp_username ? ' by ' . $aiwp_username : '';
-				$message .= ' to join a videochat happening now at ' . $aiwp_room_url . '?appear-in=' . $aiwp_room . '&aiwp-ref=invite';
-
-				// send each message separately
-				foreach ( $aiwp_invites as $email ) {
-					
-					// send the message
-					$email_sent = wp_mail( $email, apply_filters( 'aiwp_invitation_subject', 'Invitation to Appear In', $aiwp_room_type ), apply_filters( 'aiwp_invitation_message', $message, $aiwp_room_type ), $header );
-					
-					// if send fails, return false triggering error message
-					// otherwise, add 1 to invite count stats
-					if ( ! $email_sent ) {
-						echo FALSE;
-						die();
-					} else {
-						$aiwp_stats = get_option( 'aiwp_stats' );
-						$aiwp_stats[ $aiwp_room_type ]['invites_sent'] ++;
-						update_option( 'aiwp_stats', $aiwp_stats );
-					}
-				}
-
-				// return success to enter room
-				echo TRUE;
+			else {
+				$dec -= abs($diff);			
 			}
-		} else {
-			echo FALSE;
+			$dec = max(0, min(255, $dec));
+			$hex = str_pad(dechex($dec), 2, '0', STR_PAD_LEFT);
 		}
-
-		die();
-
-	}
-
-	/**
-	 * Callback for ajax counting rooms triggered
-	 *
-	 * @since    1.0
-	 */
-	public function count_session() {
-		if ( check_ajax_referer( 'aiwp-action-on_' . get_option( 'aiwp_public_room' ), 'aiwp_security' ) ) {
-			$aiwp_room_type = $_POST['aiwp_room_type'];
-			$aiwp_stats = get_option( 'aiwp_stats' );
-			$aiwp_stats[ $aiwp_room_type ]['rooms_triggered'] ++;
-			update_option( 'aiwp_stats', $aiwp_stats );
-			echo TRUE;
-		} else {
-			echo FALSE;
-		}
-		die();
-	}
-
-	/**
-	 * Callback for ajax counting accepted invites
-	 *
-	 * @since    1.0
-	 */
-	public function count_direct_session() {
-		if ( check_ajax_referer( 'aiwp-action-on_' . get_option( 'aiwp_public_room' ), 'aiwp_security' ) ) {
-
-			$aiwp_room = $_POST['aiwp_room'];
-			$aiwp_ref = $_POST['aiwp_ref'];
-			if ( strpos( $aiwp_room, '://' ) > 0 ) {
-				$aiwp_room_type = 'post';
-			} elseif ( strpos( ' ' . $aiwp_room, 'private-' ) > 0 ) {
-				$aiwp_room_type = 'private';
-			} else {
-				$aiwp_room_type = 'public';
-			}
-			$aiwp_stats = get_option( 'aiwp_stats' );
-
-			if ( 'invite' == $aiwp_ref ) {
-				$aiwp_stats[ $aiwp_room_type ]['invites_accepted'] ++;
-			}
-			update_option( 'aiwp_stats', $aiwp_stats );
-			echo TRUE;
-		} else {
-			echo FALSE;
-		}
-		die();
+	 
+		return '#'.implode($rgb);
 	}
 
 } // end class
@@ -463,9 +293,6 @@ function aiwp_include( $args ) {
 	$aiwp_defaults = array(
 		'room' => '',
 		'type' => 'public',
-		'public_invites' => NULL,
-		'private_invites' => NULL,
-		'post_invites' => NULL,
 		);
 
 	$args = wp_parse_args( $args, $aiwp_defaults );
@@ -520,4 +347,5 @@ function aiwp_expire_room() {
 }
 // Hook function to expireroom cron event (found in admin class)
 add_action( 'expireroom', 'aiwp_expire_room' );
+
 ?>
